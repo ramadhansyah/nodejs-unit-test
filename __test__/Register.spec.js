a11f549af7f99df436151760f7621aa8c29a2012
ramadhansyah <ramadhansyah.2010@gmail.com>
2022-11-27 14:35:33 +0700

    * describe

--- __test__/Register.spec.js
+++ __test__/Register.spec.js
@@ -1,28 +1,30 @@
 const request = require('supertest');
 const app = require('./../src/app');
-it('response 200 , register request is valid', (done) => {
-  request(app)
-    .post('/api/auth/register')
-    .send({
-      username: 'admin',
-      emai: 'admin@gmail.com',
-      password: 'pass123',
-    })
-    .then((response) => {
-      expect(response.status).toBe(200);
-      done();
-    });
+describe('User Registration', () => {
+  it('response 200 , register request is valid', (done) => {
+    request(app)
+      .post('/api/auth/register')
+      .send({
+        username: 'admin',
+        emai: 'admin@gmail.com',
+        password: 'pass123',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+  it('success 201 message when register request is valid', (done) => {
+    request(app)
+      .post('/api/auth/register')
+      .send({
+        username: 'admin',
+        emai: 'admin@gmail.com',
+        password: 'pass123',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('user created');
+        done();
+      });
+  });
 });
-it('success message when register request is valid', (done) => {
-  request(app)
-    .post('/api/auth/register')
-    .send({
-      username: 'admin',
-      emai: 'admin@gmail.com',
-      password: 'pass123',
-    })
-    .then((response) => {
-      expect(response.body.message).toBe('user created');
-      done();
-    });
-})
